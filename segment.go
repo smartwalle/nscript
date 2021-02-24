ef84947d35c5ffd3f4f9a4246df4d5f567e94a1e
smartwalle <smartwalle@gmail.com>
2021-02-24 14:20:02 +0800

    格式化指定添加参数

--- segment.go
+++ segment.go
@@ -213,8 +213,6 @@ func (this *inSegment) formatSay(script *Script, ctx Context, says []string) []s
 				param = matches[2]
 			}
 
-			fmt.Println(key, param, matches)
-
 			var formatCmd = getFormatCommand(key)
 			if formatCmd == nil {
 				return s
